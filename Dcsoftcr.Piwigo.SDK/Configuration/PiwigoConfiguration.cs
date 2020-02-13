c46314a3b587910c7a1b7e1881b5d4ce8cb80167
Jonathan <deadcat_v2@hotmail.com>
2020-02-13 14:46:07 -0600

    Added Configuration and Helpers

---
+++ Dcsoftcr.Piwigo.SDK/Configuration/PiwigoConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dcsoftcr.Piwigo.SDK.Configuration
+{
+    public class PiwigoConfiguration
+    {
+        public string BaseURL { get; set; }
+
+        public string WSURL { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ Dcsoftcr.Piwigo.SDK/Helpers/ImageUploaderHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Dcsoftcr.Piwigo.SDK.Configuration;
+using Dcsoftcr.Piwigo.SDK.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Dcsoftcr.Piwigo.SDK.Helpers
+{
+    public class ImageUploaderHelper
+    {
+        public static async Task<string> UploadImage(IFormFile file, string id, IPWGImages images, IPWGSession session, PiwigoConfiguration piwigoConfiguration)
+        {
+            var status = await session.GetStatus();
+            string resul;
+            if (!status.Contains(piwigoConfiguration.UserName))
+            {
+                resul = await session.LogIn(piwigoConfiguration.UserName, piwigoConfiguration.Password);
+            }
+            else
+            {
+                resul = status;
+            }
+
+            if (resul.Contains("ok"))
+            {
+                return await images.AddSimple(file, id);
+            }
+
+            return null;
+        }
+    }
+}
